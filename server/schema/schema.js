b598b0f822093f91f1b67e8badf3260d426c7a66
brianmureithi <brianmurithi65@gmail.com>
2023-03-05 08:38:19 +0300

    Add project object type with clienttype child

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,6 @@
 const {projects, clients } = require('../sampleData.js')
 
-const {GraphQLObjectType,GraphQLID, GraphQLString,GraphQLSchema}=require('graphql')
+const {GraphQLObjectType,GraphQLID, GraphQLString,GraphQLSchema,GraphQLList}=require('graphql')
 
 /* Client Type */
 
@@ -14,9 +14,46 @@ const ClientType = new GraphQLObjectType({
 
     })
 })
+const ProjectType = new GraphQLObjectType({
+    name:'Project',
+    fields:()=>({
+        id:{type:GraphQLID},
+        name:{type:GraphQLString},
+        description:{type:GraphQLString},
+        status:{type:GraphQLString},
+        client:{
+            type:ClientType,
+            resolve(){
+                
+            }
+        }
+
+    })
+})
 const RootQuery = new GraphQLObjectType({
     name:'RootQueryType',
     fields:{
+        projects:{
+        type:new GraphQLList(ProjectType),
+        resolve(parents,args){
+            return projects
+        }
+        },
+      project:{
+        type:ProjectType,
+        args:{id:{type:GraphQLID}
+      },
+      resolve(parent,args){
+        return projects.find(project=>project.id ===args.id)
+
+      }  
+    },
+        clients:{
+        type:new GraphQLList(ClientType),
+        resolve(parents,args){
+            return clients
+        }
+        },
       client:{
         type:ClientType,
         args:{id:{type:GraphQLID}
